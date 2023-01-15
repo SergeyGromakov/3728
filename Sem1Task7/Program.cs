32b94e192016ddb25c35b0f06867f00a1b8a99ab
Sergey Gromakov <sisolin@icloud.com>
2023-01-15 21:42:37 +0300

    задача 7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+string? imputNumLine = Console.ReadLine();
+// // Проверяем, чтобы данные не были пустыми
+if (imputNumLine != null)
+    {
+    // // Парсим введенное число
+    int imputNumLine2 = int.Parse(imputNumLine);
+// Берем остаток от деления 10
+int lastDigit = imputNumLine2 % 10;
+Console.WriteLine(lastDigit);
+    }
