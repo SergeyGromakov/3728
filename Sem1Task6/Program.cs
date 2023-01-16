4860034c5b2090f9d68bcc538752358a12d3a448
Sergey Gromakov <sisolin@icloud.com>
2023-01-17 00:16:05 +0300

    Task 6 Homework

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿//вводим число
+Console.Write("Введите чиcло: ");
+string? inputNum = Console.ReadLine();
+// Парсим значения 
+int number = int.Parse(inputNum);
+if (number % 2 == 0)
+{
+    Console.WriteLine("Введенное число чётное");
+}
+else
+{
+    Console.WriteLine("Введенное число нечётное");
+}
