fe14a4384ea630b1531b0b4db3042e06cac85540
Sergey Gromakov <sisolin@icloud.com>
2023-01-16 23:26:12 +0300

    Task 2 Seminar 1 Homework

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое чило: ");
+string? inputNum1 = Console.ReadLine();
+Console.Write("Введите второе чило: ");
+string? inputNum2 = Console.ReadLine();
+int number1 = int.Parse(inputNum1);
+int number2 = int.Parse(inputNum2);
+Console.Write("Больше число ");
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.WriteLine(number2);
+}
