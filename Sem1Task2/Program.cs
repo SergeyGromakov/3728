3ab47887a970481706c2ab67f522c03689c1d03c
Sergey Gromakov <sisolin@icloud.com>
2023-01-16 23:31:36 +0300

    ;

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -1,10 +1,13 @@
-﻿Console.Write("Введите первое чило: ");
+﻿//вводим 1 и второе число
+Console.Write("Введите первое чило: ");
 string? inputNum1 = Console.ReadLine();
 Console.Write("Введите второе чило: ");
 string? inputNum2 = Console.ReadLine();
+//Парсим значения 
 int number1 = int.Parse(inputNum1);
 int number2 = int.Parse(inputNum2);
 Console.Write("Больше число ");
+// Через функцию "если условие...в другом случае..." выводим большее число на экран
 if (number1 > number2)
 {
     Console.WriteLine(number1);
