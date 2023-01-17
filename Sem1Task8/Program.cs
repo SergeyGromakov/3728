08daa738b1c0fbcd6d2af472d875e47c8ff94e8d
Sergey Gromakov <sisolin@icloud.com>
2023-01-17 23:15:15 +0300

    Task 8 Homework

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿//вводим число
+Console.Write("Введите чиcло: ");
+string? inputNum = Console.ReadLine();
+if (inputNum!= null)
+{
+    int number = int.Parse(inputNum);
+    string result = string.Empty;
+    for( int i=2; i<number; i=i+2)
+    {
+        result = result + i + ",";
+    }
+    if (number%2 == 0)
+    {
+        result = result + number;
+    }
+    Console.WriteLine(result);
+}
