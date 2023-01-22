ab63e3b86c0d7865848ef98fb469a5017b456f16
Sergey Gromakov <sisolin@icloud.com>
2023-01-22 22:22:05 +0300

    Added tasks 10. 13. 15

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// // Вариант 1
+// Console.Write("Введите любое число от 100 до 999: ");
+// int number = int.Parse(Console.ReadLine() ?? "0");
+// if ((number < 100) || (number > 999))
+// {
+//     Console.WriteLine("Вы ввели неверное число");
+// }
+// else
+// преобразуем тип int в string, чтобы разбить число на 3 цифры в виде массива и выводим второе чило массива в консоль
+// {
+//     string digits = number.ToString();
+//     char[] DigitsArray = digits.ToCharArray();
+//     Console.WriteLine("Вторая цифра введенного Вами числа это  " + DigitsArray[1]);
+// }
+
+// Вариант 2
+Console.Write("Введите любое число от 100 до 999: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if ((number < 100) || (number > 999))
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+else
+{
+    // делением на 10 введенного числа избавляемся от последней цифры, 
+    // далее еще раз делим на 100 и остаток и есть вторая цифра
+    int result = number / 10;
+    result = result % 10;
+    Console.WriteLine("Вторая цифра введенного Вами числа это  " + result);
+}
+
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Вариант 1
+// Console.Write("Введите номер дня недели - ");
+// int numberofday = int.Parse(Console.ReadLine() ?? "0");
+// if ((numberofday > 0) && (numberofday < 8))
+// {
+//     if (numberofday > 5)
+//     {
+//         Console.WriteLine("Данный день является выходным");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Данный день является рабочим");
+//     }
+// }
+// else
+// {
+// Console.WriteLine("Не является днем недели");
+// }
+
+//вариант 2
+
+Console.Write("Введите номер дня недели - ");
+int numberofday = int.Parse(Console.ReadLine() ?? "0");
+if ((numberofday > 0) && (numberofday < 8))
+{
+Dictionary<int, string> daysoftheweek = new Dictionary<int, string>();
+    daysoftheweek.Add(1, "Понедельник, рабочий день");
+    daysoftheweek.Add(2, "Вторник, рабочий день");
+    daysoftheweek.Add(3, "Среда, рабочий день");
+    daysoftheweek.Add(4, "Четверг, рабочий день");
+    daysoftheweek.Add(5, "Пятница, рабочий день");
+    daysoftheweek.Add(6, "Суббота, выходной день день");
+    daysoftheweek.Add(7, "Воскресенье, выходной день день");
+Console.WriteLine(daysoftheweek[numberofday]);
+}
+else
+{
+Console.WriteLine("Не является днем недели");
+}
+
