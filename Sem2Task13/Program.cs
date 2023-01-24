a24be54b3d4e0c66111a8d92d205297de9045139
Sergey Gromakov <sisolin@icloud.com>
2023-01-25 00:18:08 +0300

    added tasks 19, 21, 23

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,50 +1,50 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // Вариант 1
-// Console.Write("Введите произвольное целое чило:  ");
+Console.Write("Введите произвольное целое чило:  ");
 // int number = int.Parse(Console.ReadLine() ?? "0");
 // string Digits = number.ToString();
-// char[] DigitsArray = Digits.ToCharArray();
-// if(DigitsArray.Length>2)
-// {
-//     Console.WriteLine("Третья Цифра числа: " + DigitsArray[2]);
-// }
-// else
-// {
-//     Console.WriteLine("Третьей цифры в числе нет!");
-// }
-
-// Вариант 2
-
-Console.Write("Введите произвольное целое чило:  ");
-int number = int.Parse(Console.ReadLine() ?? "0");
-// Ввожу дополнительную цифру, аналогичную введенной, чтобы ее использовать в счетчике, т.к. значение number 
-// понадобится далее. Андрей, я помню, что как-то можно возвратить изначальное значение переменной, но за 1,5 часа
-// я так и не смог вспомнить)))) 
-int numberforcount = number;
-int count = 0;
-// делим число на 10 до тех пор, пока остаток от деления не станет равным 0 и каждое такое деление считаем. 
-// Сумма делений = кол-во цифр
-while (numberforcount > 0)
+char[] DigitsArray = int.Parse(Console.ReadLine()?? "0").ToString().ToCharArray();
+if(DigitsArray.Length>2)
 {
-    numberforcount = numberforcount / 10;
-    count++;
-}
-Console.WriteLine("Введенное число состоит из " + count + " цифр");
-
-if (count > 3)
-{   int number1 = number / (int)Math.Pow(10, count - 3);
-    number1 = number1 % 10;
-    Console.WriteLine("Третья цифра в числе - " + number1);
+    Console.WriteLine("Третья Цифра числа: " + DigitsArray[2]);
 }
 else
 {
-    if (count == 3) 
-    {
-        int number1 = number % 10;
-        Console.WriteLine("Третья цифра в числе - " + number1);
-    }
-    else
-    {
-        Console.WriteLine("Третьей цифры в числе нет!");
-    }
+    Console.WriteLine("Третьей цифры в числе нет!");
 }
+
+// // Вариант 2
+
+// Console.Write("Введите произвольное целое чило:  ");
+// int number = int.Parse(Console.ReadLine() ?? "0");
+// // Ввожу дополнительную цифру, аналогичную введенной, чтобы ее использовать в счетчике, т.к. значение number 
+// // понадобится далее. Андрей, я помню, что как-то можно возвратить изначальное значение переменной, но за 1,5 часа
+// // я так и не смог вспомнить)))) 
+// int numberforcount = number;
+// int count = 0;
+// // делим число на 10 до тех пор, пока остаток от деления не станет равным 0 и каждое такое деление считаем. 
+// // Сумма делений = кол-во цифр
+// while (numberforcount > 0)
+// {
+//     numberforcount = numberforcount / 10;
+//     count++;
+// }
+// Console.WriteLine("Введенное число состоит из " + count + " цифр");
+
+// if (count > 3)
+// {   int number1 = number / (int)Math.Pow(10, count - 3);
+//     number1 = number1 % 10;
+//     Console.WriteLine("Третья цифра в числе - " + number1);
+// }
+// else
+// {
+//     if (count == 3) 
+//     {
+//         int number1 = number % 10;
+//         Console.WriteLine("Третья цифра в числе - " + number1);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Третьей цифры в числе нет!");
+//     }
+// }
---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Метод ввода данных пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// проверяем является ли число палиндромом
+
+bool PalinTest(int n)
+{
+    bool res = false;
+    int d1 = n / 10000;
+    int d2 = (n / 1000) % 10;
+    int d3 = (n / 10) % 10;
+    int d4 = n % 10;
+    res = ((d1 == d4) && (d2 == d3)) ? true : false;
+    return res;
+}
+// выводим результат на основании данных об истинности и сравниваем число
+void PrintData(int num, bool res)
+{
+    if ((num > 9999) && (num < 100000))
+    {
+        if (res == true) Console.WriteLine("Введенное Вами число является палиндромом");
+        if (res == false) Console.WriteLine("Введенное Вами число не является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Введенное Вами число не является пятизначным");
+    }
+}
+
+int FiveDigitsNumber = ReadData("Введите пятизначное число: ");
+bool TrueOrFalse = PalinTest(FiveDigitsNumber);
+PrintData(FiveDigitsNumber, TrueOrFalse);
