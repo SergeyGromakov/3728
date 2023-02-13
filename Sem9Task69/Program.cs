1b47a5145a3bb6d056d81fa77a1544395d011913
Sergey Gromakov <sisolin@icloud.com>
2023-02-14 00:31:52 +0300

    Added 63, 65, 67, 69

---
+++ Sem9Task69/Program.cs
@@ -0,0 +1,31 @@
+﻿// Принимает а и б и возводит а в степень б
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintResult(long prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+long RecPowFlow(int a, int b)
+{
+    if(b<=1)return a;
+    return a*RecPowFlow(a,b-1);
+}
+
+long MyPow(int a, int b)
+{
+    if(b==2)return a*a;
+    return MyPow(a,b/2)*MyPow(a,b/2);
+}
+
+int numberA = ReadData("Enter number A:");
+int numberB = ReadData("Enter number B:");
+long res1 =0;
+long res2 =0;
+PrintResult(res1);
+PrintResult(res2);
